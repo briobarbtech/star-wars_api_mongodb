c196aa21f4832e6414172eabbaa459ab28f46987
Brian Barquesi <barquesi.tech@gmail.com>
2022-11-04 18:49:48 -0300

    microservices

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from models.report import Report
 ### Inicializo Flask
 app = Flask(__name__)
 ### Defino una propiedad que va a decir donde buscar MongoDB. MongoDB siempre trabaja en el puerto 27017
-app.config['MONGO_URI']='mongodb://localhost/starwarsdb'
+app.config['MONGO_URI']='mongodb://mydb/starwarsdb'
 ### Le paso la configuración de mi app a PyMongo
 mongo = PyMongo(app)
 ### to report Post request to http://localhost:5000/report
@@ -31,22 +31,27 @@ def get_reports():
 
 @app.route('/character', methods=['POST'])
 def create_character():
-    id = mongo.db.character.insert_one(request.json)
+    id = mongo.db.characters.insert_one(request.json)
     response = {"id" : str(id)}
     return response,http.HTTPStatus.CREATED
 
 ### to get all characters Get request to http://localhost:5000/characters
 @app.route('/characters', methods=["GET"])
 def index():
-    characters = mongo.db.character.find()
+    characters = mongo.db.characters.find()
     response = json_util.dumps(characters)
     return Response(response, mimetype='application/json'),http.HTTPStatus.OK
 
 ### to get characters by id Get request to http://localhost:5000/characters/<id>
 @app.route('/characters/<id>', methods=['GET'])
 def getPage(id):
-    character = mongo.db.character.find_one({'_id': ObjectId(id)})
+    character = mongo.db.characters.find_one({'_id': ObjectId(id)})
     response = json_util.dumps(character)
+    return Response(response, mimetype='application/json'),http.HTTPStatus.OK
+@app.route('/characters', methods=['DELETE'])
+def deleteAllcharacters():
+    mongo.db.characters.drop()
+    response = mongo.db.characters.find()
     return Response(response, mimetype='application/json'),http.HTTPStatus.OK
 @app.errorhandler(404)
 
@@ -56,4 +61,4 @@ def not_found(error=None):
     message = {'message': 'Resource Not Found: ' + request.url,'status' : 404}
     return message
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
